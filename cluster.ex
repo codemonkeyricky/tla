80700651b39b6206669b8965107db5a04b9f04f6
Richard Tang <code.monkey.ricky@gmail.com>
2025-02-25 10:24:17 -0800

    snapshot

--- cluster.ex
+++ cluster.ex
@@ -1,20 +1,46 @@
 defmodule Cluster do
   def start do
-    epoch_pid = spawn(__MODULE__, :rg, [%{}])
+    n0 = spawn(__MODULE__, :rg, [%{}])
+    n1 = spawn(__MODULE__, :rg, [%{}])
 
-    send(epoch_pid, {:epoc})
+    send(n0, {:epoch})
+    send(n1, {:init, n0})
+  end
+
+  defp merge_peers(local, remote) do
+    Map.merge(local, remote,
+      fn _token, local_node, remote_node->
+        if local_node.version >= remote_node.version do
+          local_node
+        else
+          remote_node
+        end
+      end)
   end
 
   # Replica group definition
   def rg(peers) do
     receive do
-      {:epoc} ->
-
-      # token -> {pid, version}
-      updated_peers = Map.put(peers, 0, %{pid: self(), version: 1})
-      IO.puts("updated_peers: #{inspect(updated_peers)}")
-
+      {:epoch} ->
+      updated_peers = Map.put(peers, 0, %{pid: self(), state: Online, version: 1})
+      IO.puts("epoch: #{inspect(updated_peers)}")
       rg(updated_peers)
+
+      {:init, peer_pid} ->
+        IO.puts("init: #{inspect(self())}")
+        send(peer_pid, {:request_peers, self()})
+        receive do
+          {:peers, remote_peers} ->
+            merged_peers = merge_peers(peers, remote_peers)
+            IO.puts("init merged: #{inspect(self())}")
+            rg(merged_peers)
+        end
+
+      {:request_peers, request_pid} ->
+        IO.puts("request_peers: #{inspect(self())}")
+        send(request_pid, {:peers, peers})
+        rg(peers)
+
     end
 
     rg(peers)
