4f15cd03342eee07478eb522a08bdeddb7cc08b5
Richard Tang <code.monkey.ricky@gmail.com>
2025-02-25 12:29:22 -0800

    snapshot

--- cluster.ex
+++ cluster.ex
@@ -1,7 +1,7 @@
 defmodule Cluster do
   def start do
-    n0 = spawn(__MODULE__, :rg, [-1, %{}])
-    n1 = spawn(__MODULE__, :rg, [-1, %{}])
+    n0 = spawn(__MODULE__, :rg, [%{}, %{}])
+    n1 = spawn(__MODULE__, :rg, [%{}, %{}])
 
     send(n0, {:epoch})
     send(n1, {:init, n0})
@@ -23,42 +23,44 @@ defmodule Cluster do
   # end
 
   # Replica group definition
-  def rg(token, local_ring) do
+  def rg(property, local_ring) do
     receive do
 
       {:epoch} ->
-        updated_token = 0
-        updated_local_ring = Map.put(local_ring, updated_token, %{pid: self(), state: Online, version: 1})
+        updated_property = %{token: 0}
+        updated_local_ring = Map.put(local_ring, 0, %{pid: self(), state: Online, version: 1})
         IO.puts("epoch: #{inspect(updated_local_ring)}")
-        rg(updated_token, updated_local_ring)
+        rg(updated_property, updated_local_ring)
 
       {:init, peer_pid} ->
         IO.puts("init: #{inspect(self())}")
-        updated_token = :rand.uniform(32)
-        local_ring = Map.put(local_ring, updated_token, %{pid: self(), state: Joining, version: 1})
+        token = :rand.uniform(32)
+        updated_property = %{token: token}
+        local_ring = Map.put(local_ring, token , %{pid: self(), state: Joining, version: 1})
         send(peer_pid, {:gossip_req, self(), local_ring})
-        rg(updated_token, local_ring)
+        rg(updated_property, local_ring)
 
       {:gossip_ack, remote_ring} ->
         IO.puts("#{inspect(self())}: gossip_ack")
         merged_local_ring = merge_ring(local_ring, remote_ring)
         send(self(), {:heartbeat})
-        rg(token, merged_local_ring)
+        rg(property, merged_local_ring)
 
       {:gossip_req, remote_pid, remote_ring} ->
         IO.puts("#{inspect(self())}: gossip_req")
         merged_local_ring = merge_ring(local_ring, remote_ring)
         send(remote_pid, {:gossip_ack, merged_local_ring})
         send(self(), {:heartbeat})
-        rg(token, merged_local_ring)
+        rg(property, merged_local_ring)
 
       {:heartbeat} ->
         # IO.puts("#{inspect(self())}: heartbeat")
         keys = local_ring |> Map.keys() |> Enum.sort()
-        # IO.puts("#{inspect(self())}: heartbeat: #{inspect(keys)}")
+        IO.puts("#{inspect(self())}: heartbeat: #{inspect(keys)}")
         # my_token = local_ring.
-        key_index = Enum.find_index(keys, fn k -> k == token end)
-        IO.puts("#{inspect(self())}: heartbeat: token #{inspect(token)}")
+        key_index = Enum.find_index(keys, fn k -> k == property.token end)
+        IO.puts("#{inspect(self())}: heartbeat: token #{inspect(property.token)}")
+        # IO.puts("#{inspect(self())}: heartbeat: key_index #{inspect(key_index)}")
         # IO.puts("#{inspect(self())}: heartbeat: token index #{inspect(key_index)}")
         prev_token =
           case key_index do
@@ -71,7 +73,7 @@ defmodule Cluster do
         #   end
     end
 
-    rg(token, local_ring)
+    rg(property, local_ring)
   end
 end
 
